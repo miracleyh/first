c859730fa4c1bb5a6f6fef729234fbe43e6c4180
apple <3129994943@qq.com>
2020-01-26 12:34:37 +0800

    第一次版本

---
+++ first.py
@@ -0,0 +1,14 @@
+#-*-coding:utf-8-*-
+my_list = ["jake", "Tom", "xi li"]
+
+# for name in my_list:
+#     print(name)
+
+# for number in range(11):
+#     print(number)
+
+
+for letter in 'inty':
+    print("each letter is: " + letter)
+
+'''这是miracle的first demo'''
